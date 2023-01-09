a0ec03e6afeaba6c919d67b026b6aeba7074fd8e
Piotr Regulski <bulu901@gmail.com>
2023-01-09 20:11:33 +0100

    add function random in to span

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 
+const toPayspan =document.querySelector("#to-pay");
+const paidSpan= document.querySelector("#paid");
+
+const cash = toPayspan.innerHTML=getRandomIntInclusive(1,100);
+paidSpan.innerHTML=getRndInteger(Number(cash), 100);
+
+
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min)}
+   
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+      }   
+
+
+Number(cash);
+console.log(Number(cash));
+
+
